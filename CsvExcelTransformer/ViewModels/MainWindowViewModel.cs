2a995fc5fed66c4d2e65b731b076be7a2354c642
Brian Pendleton <brian.pendleton@radiancetech.com>
2022-07-14 12:27:51 -0500

    FIrst batch of updates.

---
+++ CsvExcelTransformer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Timers;
+using CsvExcelTransformer.Classes;
+using WpfPageTransitions;
+
+namespace CsvExcelTransformer.ViewModels
+{
+    public class MainWindowViewModel : NotifyObject
+    {
+        #region Private Members
+        
+        private const string m_BaseTitle = " CSV Excel Transformer";
+        private PageTransition m_PageTransition;
+
+        private const int MAX_STATUS_TEXT_COUNT = 8;
+        private int m_StatusTextCurrentCount = 0;
+        private readonly Timer m_StatusTextTimer = new Timer();
+
+        private bool m_ShowProgressBar = false;
+        private string m_StatusText = string.Empty;
+        private string m_TitleText = m_BaseTitle;
+
+        #endregion Private Members
+
+        #region Public Properties
+
+        public bool ShowProgressBar
+        {
+            get => m_ShowProgressBar;
+
+            set
+            {
+                if (m_ShowProgressBar != value)
+                {
+                    m_ShowProgressBar = value;
+
+                    RaisePropertyChanged(nameof(ShowProgressBar));
+                }
+            }
+        }
+
+        public string StatusText
+        {
+            get => m_StatusText;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(m_StatusText) ||
+                    m_StatusText.Equals(value, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    m_StatusText = value;
+
+                    RaisePropertyChanged(nameof(StatusText));
+                }
+            }
+        }
+
+        public string TitleText 
+        { 
+            get => m_TitleText;
+
+            internal set
+            {
+                if (string.IsNullOrWhiteSpace(m_TitleText) ||
+                    m_TitleText != value)
+                {
+                    m_TitleText = value;
+
+                    RaisePropertyChanged(nameof(TitleText));
+                }
+            }
+        }
+
+        #endregion Public Properties
+
+        #region constructor
+
+        internal MainWindowViewModel(PageTransition pageTransitionControl)
+        {
+            m_PageTransition = pageTransitionControl;
+
+            m_StatusTextTimer.Interval = 1000.0d;
+            m_StatusTextTimer.Elapsed += StatusTextTimer_Elapsed;
+        }
+
+        #endregion constructor
+
+        #region Private Methods
+
+        private void StatusTextTimer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            if ((++m_StatusTextCurrentCount) >= MAX_STATUS_TEXT_COUNT)
+            {
+                m_StatusTextTimer.Stop();
+
+                StatusText = string.Empty;
+            }
+        }
+
+        #endregion Private Methods
+
+        #region Public Methods
+
+        internal void SetStatusText(string text, bool autoRemove = true)
+        {
+            StatusText = text;
+
+            if (autoRemove)
+            {
+                m_StatusTextTimer.Stop();
+
+                m_StatusTextCurrentCount = 0;
+
+                m_StatusTextTimer.Start();
+
+            }
+        }
+
+        #endregion Public Methods
+
+    }
+}
---
+++ CsvExcelTransformer/Views/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CsvExcelTransformer.ViewModels;
+
+namespace CsvExcelTransformer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private Members
+
+        private readonly MainWindowViewModel m_ViewModel;
+
+        #endregion Private Members
+
+        #region constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            m_ViewModel = new MainWindowViewModel(pageTransitionControl);
+            m_ViewModel.TitleText = Title;
+
+            DataContext = m_ViewModel;
+        }
+
+        #endregion constructor
+
+        #region Private Methods
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion Private Methods
+
+        #region Public Methods
+
+        internal void SetStatusText(string statusText)
+        {
+            m_ViewModel.StatusText = statusText;
+        }
+
+        #endregion Public Methods
+
+    }
+}
